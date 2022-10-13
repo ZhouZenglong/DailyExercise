3f2fd9fb8c385babc398d1c8dc1d5cf3ba8db9f2
ZhouZenglong <113286294+ZhouZenglong@users.noreply.github.com>
2022-10-13 16:51:58 +0800

    socket的使用

---
+++ socket.py
@@ -0,0 +1,13 @@
+import socket
+
+mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mysock.connect(('10.20.133.78',80))
+cmd = 'GET http://10.20.133.78/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(521)
+    if (len(data) <1):
+        break
+    print(data.decode(),end = '')
+mysock.close()
